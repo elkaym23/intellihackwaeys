6bc0c716a3863e899a0c6ff10b5243198e03740a
elkaym23 <elkaymiller@gmail.com>
2025-03-16 05:17:01 -0500

    dashboards

--- group-accountability-app/src/AssignTask.js
+++ group-accountability-app/src/AssignTask.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import supabase from "../supabaseClient";
+import supabase from "./supabaseClient";
 
 const AssignTask = ({ projectId }) => {
     const [taskName, setTaskName] = useState("");
