7ee8c2b1ae4270780c6b35a291623916cc896aba
elkaym23 <elkaymiller@gmail.com>
2025-03-15 22:08:24 -0500

    i am commiting

--- group-accountability-app/src/supaBaseclient.js
+++ group-accountability-app/src/supaBaseclient.js
@@ -5,4 +5,5 @@ const SUPABASE_ANON_KEY = "process.env.SUPABASE_KEY"; // Replace with your actua
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-
+console.log("SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("SUPABASE_KEY:", process.env.REACT_APP_SUPABASE_KEY);
