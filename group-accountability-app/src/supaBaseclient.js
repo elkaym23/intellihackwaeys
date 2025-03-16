b97dc6c0334abe943fc56c1056dacd5891816091
elkaym23 <elkaymiller@gmail.com>
2025-03-16 05:51:56 -0500

    supa

--- group-accountability-app/src/supaBaseclient.js
+++ group-accountability-app/src/supaBaseclient.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wsmqmppgxojpvwqkhcev.supabase.co"; // Replace with your actual Supabase project URL
-const SUPABASE_ANON_KEY = "process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzbXFtcHBneG9qcHZ3cWtoY2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNTg2ODgsImV4cCI6MjA1NzYzNDY4OH0.9WemPPuN9gddae14piHypgmxW99spRrz9JUWKQL2Zvo.env.SUPABASE_KEY"; // Replace with your actual Supabase anon key
+const SUPABASE_URL = "https://wsmqmppgxojpvwqkhcev.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzbXFtcHBneG9qcHZ3cWtoY2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNTg2ODgsImV4cCI6MjA1NzYzNDY4OH0.9WemPPuN9gddae14piHypgmxW99spRrz9JUWKQL2Zvo"; // Replace with your actual Supabase anon key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-console.log("SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
-console.log("SUPABASE_KEY:", process.env.REACT_APP_SUPABASE_KEY);
