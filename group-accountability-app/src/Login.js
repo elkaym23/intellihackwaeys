f0cb587ce6b810eb5388e98e825a179d55900ada
elkaym23 <elkaymiller@gmail.com>
2025-03-16 09:12:00 -0500

    SelectRoles

--- group-accountability-app/src/Login.js
+++ group-accountability-app/src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from './supaBaseclient';
-import './Login.css';
+import './login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
--- group-accountability-app/src/SelectRoles.js
+++ group-accountability-app/src/SelectRoles.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from './supaBaseclient';
-import './Login.css';
+import './login.css';
 
 const SelectRole = () => {
     const [role, setRole] = useState('');
